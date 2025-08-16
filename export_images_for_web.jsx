ac0b82f582a6b9eea2cf70acc28c5c28f3c6765b
Ricardo Merlos Torres <spectra502@gmail.com>
2025-08-16 12:29:52 +0200

    adapted multiple selection of files instead of the whole folder

--- export_images_for_web.jsx
+++ export_images_for_web.jsx
@@ -1,46 +1,61 @@
-var inputFolder = Folder.selectDialog("Select the folder with PSD files");
+// Pick specific PSD files (multi-select)
+var inputFiles = File.openDialog(
+    "Select one or more PSD files", 
+    "Photoshop files:*.psd;*.psb", 
+    true // <- allow multi-select
+);
+
+// Still pick an output folder
 var outputFolder = Folder.selectDialog("Select the output folder for JPEGs");
 
-var targetGroupName = "aretes_editados"; // Name of the group to rotate
-var rotateAngle = 15; // Angle to rotate the target group or layer
+// Bail if user cancels
+if (!inputFiles || !outputFolder) {
+    // user cancelled
+} else {
+    // If only one file was selected, File.openDialog returns a File (not an Array)
+    if (!(inputFiles instanceof Array)) inputFiles = [inputFiles];
 
-if (inputFolder != null && outputFolder != null) {
-    var files = inputFolder.getFiles("*.psd");
+    var targetGroupName = "aretes_editados"; // Name of the group/layer to rotate
+    var rotateAngle = 15; // Angle to rotate
 
-    for (var i = 0; i < files.length; i++) {
-        var doc = open(files[i]);
-
-        // Prepare output filename
-        var fileName = doc.name.replace(/\.[^\.]+$/, ''); // remove extension
-        var outFile1 = new File(outputFolder + "/" + fileName + "-01"+ ".jpg");
-        var outFile2 = new File(outputFolder + "/" + fileName + "-02"+ ".jpg");
-
-        // Save File1 (normal position) for Web options
-        var opts = new ExportOptionsSaveForWeb();
-        opts.format = SaveDocumentType.JPEG;
-        opts.optimized = true;
-        opts.quality = 100;
-        opts.includeProfile = false; // false means it will convert to sRGB
-
-        doc.exportDocument(outFile1, ExportType.SAVEFORWEB, opts);
-
-        // Save File2 (rotated position) for Web options
+    for (var i = 0; i < inputFiles.length; i++) {
+        var doc = open(inputFiles[i]);
         try {
-            var target = doc.layerSets.getByName(targetGroupName);
-        } catch (e) {
-            // fall back to layer
+            // Prepare output filenames
+            var fileName = doc.name.replace(/\.[^\.]+$/, ''); // remove extension
+            var outFile1 = new File(outputFolder + "/" + fileName + "-01.jpg");
+            var outFile2 = new File(outputFolder + "/" + fileName + "-02.jpg");
+
+            // Save File1 (normal position) - Save for Web
+            var opts = new ExportOptionsSaveForWeb();
+            opts.format = SaveDocumentType.JPEG;
+            opts.optimized = true;
+            opts.quality = 100;
+            opts.includeProfile = false;
+
+            doc.exportDocument(outFile1, ExportType.SAVEFORWEB, opts);
+
+            // Find target group first; if not, try a layer
+            var target;
             try {
-                target = doc.artLayers.getByName(targetGroupName);
-            } catch (e2) {
-                alert("Group or layer '" + targetGroupName + "' not found in " + doc.name);
-                continue;
+                target = doc.layerSets.getByName(targetGroupName);
+            } catch (e) {
+                try {
+                    target = doc.artLayers.getByName(targetGroupName);
+                } catch (e2) {
+                    alert("Group or layer '" + targetGroupName + "' not found in " + doc.name);
+                    continue; // skip this doc
+                }
             }
+
+            // Rotate and export second image
+            doc.activeLayer = target;
+            target.rotate(rotateAngle, AnchorPosition.MIDDLECENTER);
+            doc.exportDocument(outFile2, ExportType.SAVEFORWEB, opts);
+
+        } finally {
+            // Close without saving changes
+            doc.close(SaveOptions.DONOTSAVECHANGES);
         }
-
-        doc.activeLayer = target;
-        target.rotate(rotateAngle, AnchorPosition.MIDDLECENTER); // Rotate the target group or layer by 15 degrees
-        doc.exportDocument(outFile2, ExportType.SAVEFORWEB, opts);
-
-        doc.close(SaveOptions.DONOTSAVECHANGES);
     }
 }
